621cac821449575b76cb4781c85219d07f8b48c1
liam677 <timgm.lzh@gmail.com>
2025-04-09 16:11:38 -0700

    Add files via upload

---
+++ ifte-investment-adviser-webapp/src/assembly/conf/env.sh
@@ -0,0 +1,24 @@
+APP_PORT=18181
+APP_NAME=ifte-investment-adviser-webapp
+
+if [ -f "/opt/applications/tingyun/tingyun-agent-java.jar" ];then
+  JAVA_AGENT_OPTS=" -javaagent:/opt/applications/tingyun/tingyun-agent-java.jar -Dtingyun.app_name=ifte-financial_${APP_NAME} "
+fi
+
+PROFILE=$(echo ${FOUNDERSC_ENV} | tr '[A-Z]' '[a-z]')
+case ${PROFILE} in
+  dev)
+      JAVA_OPT=" ${JAVA_AGENT_OPTS} -server -Xmx800m -Xms800m -Xmn256m -XX:MetaspaceSize=128m -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/logs/${APP_NAME}/oom.hprof "
+      ;;
+  qa)
+      JAVA_OPT=" ${JAVA_AGENT_OPTS} -server -Xmx800m -Xms800m -Xmn256m -XX:MetaspaceSize=128m -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/logs/${APP_NAME}/oom.hprof "
+      ;;
+  pre)
+      JAVA_OPT=" ${JAVA_AGENT_OPTS} -server -Xmx1g -Xms1g -Xmn500m -XX:MetaspaceSize=128m -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/logs/${APP_NAME}/oom.hprof "
+      ;;
+  product)
+      JAVA_OPT=" ${JAVA_AGENT_OPTS} -server -Xmx2g -Xms2g -Xmn768m -XX:MetaspaceSize=128m -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/logs/${APP_NAME}/oom.hprof "
+      ;;
+  *)
+      ;;
+esac
